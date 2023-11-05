57a03f806d228c9c94c87fec4e829310624a8cbe
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-05 00:45:59 -0700

    Reformat

--- app/_ui/customers/table.tsx
+++ app/_ui/customers/table.tsx
@@ -1,7 +1,10 @@
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import Search from '../search';
-import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
+import Image from "next/image";
+import { lusitana } from "@/app/_ui/fonts";
+import Search from "../search";
+import {
+  CustomersTable,
+  FormattedCustomersTable,
+} from "@/app/_lib/definitions";
 
 export default async function CustomersTable({
   customers,
